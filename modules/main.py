4256b3e4293460ca658ae575ee208512f326d8d1
Nick Lluen <98544701+nlluen@users.noreply.github.com>
2023-06-04 04:33:37 -0400

    Initial commit

---
+++ modules/main.py
@@ -0,0 +1,62 @@
+import asyncio
+import os
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+intents.message_content = True
+bot = commands.Bot(command_prefix='+', intents=intents)
+client = discord.Client(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    activity = discord.Activity(type=discord.ActivityType.watching, name="The Degenerates of this Server")
+    await bot.change_presence(activity=activity)
+    #await bot.change_presence(activity=discord.Game("with the dog inside"))
+    await bot.get_channel(1045937547597053982).send("I am alive and am capable of feeling.");
+    print("Bot is ready")
+
+
+@bot.event
+async def on_message_delete(message):
+    # Check that the message was not sent by a bot to avoid logging bot actions
+    if message.author.bot:
+        return
+
+    channel_id = message.channel.id
+    mod_log_id = 762824517654937631
+    log_channel = bot.get_channel(mod_log_id)
+    deleted_channel = bot.get_channel(channel_id)
+    # Log the deleted message to a file or database
+    embed = discord.Embed(title=f"{message.author.name}#{message.author.discriminator}",  color=0xff0000)
+    embed.add_field(name=f'Message was deleted in {message.channel.mention}', value=message.content)
+    with open('pictures/deleted.jpg', 'rb') as f:
+        image = discord.File(f)
+        # Send the image to the channel
+        await deleted_channel.send(file=image)
+    await log_channel.send(embed=embed)
+
+
+@bot.command(name='ping', help='Sends jotchua :)')
+async def ping(ctx):
+    await ctx.send('<:jotchua:992580804188319824>')
+
+
+@bot.command(name='stop', help='stops bot')
+async def stop(ctx):
+    await bot.get_channel(1045937547597053982).send("I am getting very sleepy...why is it all turning black...")
+    await bot.close()
+
+
+async def main():
+    #await load()
+    #await bot.start(os.environ['TOKEN'])
+    await bot.start('MTAzNDYwOTY5NDUwMjgzNDE5Ng.GM-rw9.GxlpjDH6DKv8nbu2OZgaswwgV77TQUKhchlw18')
+asyncio.run(main())
+
+
+
+
+
